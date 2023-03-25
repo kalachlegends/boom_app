ab99d345d5140fb07c25e013a574b08bf222d09b
Geraska <tuftedfun@gmail.com>
2023-03-25 15:59:54 +0600

    added money & money_movement

---
+++ boom_umbrella/apps/boom/lib/boom/model/money/money.ex
@@ -0,0 +1,24 @@
+defmodule Boom.Model.Money do
+  use Ecto.Schema
+  import Ecto.Changeset
+  @primary_key {:id, :binary_id, autogenerate: true}
+  @foreign_key_type :binary_id
+
+  schema "money" do
+    field(:org_id, :binary_id)
+    field(:cash, :map)
+
+    timestamps()
+  end
+
+  use Boom.Use.RepoBase, repo: Boom.Repo
+
+  @doc false
+  @required_fields ~w(org_id cash)a
+  @optional_fields ~w()a
+  def changeset(model, attrs) do
+    model
+    |> cast(attrs, @required_fields ++ @optional_fields)
+    |> validate_required(@optional_fields)
+  end
+end
