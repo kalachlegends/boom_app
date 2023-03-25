029f1a232fe139d31d0b33df92f568a11ed614f4
artem <artem.kalachev.2002@gmail.com>
2023-03-25 16:57:28 +0600

    feat: registation

--- frotend/src/pages/auth/RegisterView.vue
+++ frotend/src/pages/auth/RegisterView.vue
@@ -3,33 +3,50 @@
         <div class="login-block">
     
           <LogoForAuth />    
-        
-                 <form class="login-block__body">
-                  <n-h1>Выберите город для начало</n-h1>
-                  <SearchLocation v-model:selectedValue="selectedValue" multiple="true"/>
-                  <loader v-if="isLoad" :sx="{alignSelf: 'center'}" />
-                 <!-- <input-image v-model="dataForm.email" :placeholder="'Email'"  urlImage="img/icons/baseline-email.svg" :error="getKeyError(errors.errors, 'email')" />
-                  <input-image v-model="dataForm.login"  :placeholder="'Имя пользователя'" urlImage="img/icons/user.svg" :error="getKeyError(errors.errors, 'login')" />
-                  <input-image v-model="dataForm.password" type="password" :placeholder="'Пароль'"  urlImage="img/icons/lock-password.svg" :error="getKeyError(errors.errors, 'password')" />
-                  <input-image v-model="dataForm.repassword"  type="password"  :placeholder="'Повторите пароль'"  urlImage="img/icons/lock-password.svg" :error="getKeyError(errors.errors, 'repassword')" />
+          <form  class="login-block__body">
+            <loader v-if="isLoad" :sx="{alignSelf: 'center'}" />
+                   <Transition name="nested">
+                    <n-form class="outer">
+                      <div v-if="steps == 1" class='tw-flex tw-flex-col tw-gap-4'>
+                        <n-h1>Выберите город для начало</n-h1>
+                        <n-form-item label="Адрес" path="location.locality_id">
+                        <SearchLocation v-model:selectedValue="dataForm.location.location_id" />
+                        </n-form-item>
+                        <n-form-item label="Адрес" path="">
+                        <n-input placeholder="Адрес" size="large" v-model="dataForm.location.address" />
+                        </n-form-item>
+                       </div>
+                      <div v-if="steps == 2" class='tw-flex tw-flex-col tw-gap-4'>
+                        <n-h1>Регистрация</n-h1>
+                        <n-form-item label="ИИН" path="login">
+                        <n-input v-model:value="dataForm.login" size="large"  :placeholder="'Авторизация'" urlImage="img/icons/user.svg" />
+                        </n-form-item>
+                        <n-form-item label="Пароль" path="password">
+                          <n-input v-model:value="dataForm.password" size="large" type="password" :placeholder="'Пароль'" urlImage="img/icons/user.svg" />
+                          </n-form-item>
+                        
+                       
+                      </div>
+
+                    </n-form>
                
-                    <button @click.prevent="handleClickRegister" type="submit" class="btn-grey">
-                        Войти
-                    </button>
+               
+                </Transition>  
+                    <span class="tw-flex tw-justify-between tw-gap-10">
+                      
+                      <ArrowBack  @click="handleStep('prev')" />
+                      <span class="steps">
+                        {{steps}} / {{limit}}
+                      </span>
+                      <ArrowBack @click="handleStep('next')" class="tw-rotate-180" />
+                    </span>
                     <div class="df-aic-jc-fx-c gap-10">
-                        <router-link to="/login" class="text-align-center width-80">У вас уже есть аккаунт? Можете войти здесь</router-link>
-                        <router-link to="/restore_password" class="text-align-center width-80">Забыли пароль?</router-link>
-                    </div> -->
-                </form>  
-                <span class="tw-flex tw-justify-between tw-gap-10">
-                  {{selectedValue}}
-                  <ArrowBack />
-                  <span class="steps">
-                    {{steps}} / {{limit}}
-                  </span>
-                  <ArrowBack @click="handleStep('next')" class="tw-rotate-180" />
-                </span>
+                      <router-link to="/login" class="text-align-center width-80">У вас уже есть аккаунт? Можете войти здесь</router-link>
+                      <router-link to="/restore_password" class="text-align-center width-80">Забыли пароль?</router-link>
+                  </div> 
+                </form>
             </div> 
+            
     </div>
     </template>
 
@@ -45,12 +62,19 @@ export default {
     const { dataForm, handleClickRegister, isLoad, getKeyError, errors } =
       useRegister();
     const steps = ref(1);
-    const limit = 3;
-    const selectedValue = ref(null);
-    const handleStep = (type) => {
-      if (limit > steps.value) {
-        console.log("asd");
+    const limit = 2;
+
+    const handleStep = async (type) => {
+      if (type == "next" && limit > steps.value) {
         steps.value = steps.value + 1;
+        return;
+      }
+      if (limit == steps.value) {
+        await handleClickRegister();
+        console.log("123");
+      }
+      if (steps.value != 1 && type == "prev") {
+        steps.value = steps.value - 1;
       }
     };
     return {
@@ -62,16 +86,29 @@ export default {
       steps,
       limit,
       handleStep,
-      selectedValue,
     };
   },
 };
 </script>
     <style lang="scss" scoped>
 @import "src/css/mixins/mixins.scss";
+label {
+  color: white;
+}
 .n-h1 {
   color: white;
 }
+.nested-enter-active .tw-flex,
+.nested-leave-active .tw-flex {
+  transition: all 0.3s ease-in-out;
+}
+
+.nested-enter-from .tw-flex,
+.nested-leave-to .tw-flex {
+  transform: translateX(30px);
+  opacity: 0;
+}
+
 svg {
   color: white;
   width: 34px;
@@ -80,5 +117,8 @@ svg {
   font-size: 20px;
   font-weight: bold;
   color: white;
+}
+.login-block__body {
+  gap: 30px;
 }
 </style>
