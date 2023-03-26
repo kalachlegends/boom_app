c2d419838c34612e6234d2538406c9513491680d
chudickgumanoid <sashaq0708@gmail.com>
2023-03-26 15:33:47 +0600

    two pages

---
+++ frotend/src/pages/telegram/TelegramPage.vue
@@ -0,0 +1,22 @@
+<template>
+  <n-card
+    title="Как получить доступ к персональному Telegram каналу вашей ОСИ"
+    size="huge"
+    class="tw-flex tw-text-center tw-justify-center tw-items-center"
+  >
+    <div class="tw-text-xl tw-max-w-5xl tw-pb-8 tw-text-center tw-mx-auto">
+      Чтобы оплатить тариф на сервисе, вам необходимо установите Telegram на
+      своё мобильное устройство. Отсканируйте QR-код представленный ниже через
+      приложение
+    </div>
+    <div class="tw-max-w-sm tw-mx-auto">
+      <img class="tw-rounded-3xl" src="~/assets/img/other/qr_tg.png" alt="" />
+    </div>
+  </n-card>
+</template>
+
+<script>
+</script>
+
+<style lang="scss" scoped>
+</style>
